bdb20fd50f6d05513ee708da1faeb9941206d4a0
ToddZember <toddzember@gmail.com>
2017-05-22 00:59:06 -0400

    Most of Section 2 Completed

--- MVCAppGood/App_Start/RouteConfig.cs
+++ MVCAppGood/App_Start/RouteConfig.cs
@@ -13,11 +13,9 @@ namespace MVCAppGood
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" });
+
+            routes.MapRoute("Home", "", new {controller = "Posts", action = "Index" });
         }
     }
 }
